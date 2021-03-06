49c6c6e7c7e3223498c5d52b97185b81c4919956
slickoner <slickrrrr@gmail.com>
2021-03-06 19:41:35 +0100

    Setup project && dependencies

---
+++ src/test/java/com/krauss/application/BachelorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krauss.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
